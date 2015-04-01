03c0e66ef10713dcf236d2865339d6911bd59250
franchi <franchi@JavierF.vinfra.local>
2015-04-01 14:56:08 +0200

    01/04/2015. Starting Proyect

--- HyCrop/src/App/MainWindow.java
+++ HyCrop/src/App/MainWindow.java
@@ -3,10 +3,18 @@ package App;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+import javax.swing.JMenu;
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class MainWindow {
 
 	private JFrame MainWindow;
+	private int anchowindow = 300;
 
 	/**
 	 * Launch the application.
@@ -36,8 +44,38 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		MainWindow = new JFrame();
+		JMenuBar menuBar = new JMenuBar();
+		MainWindow.addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent arg0) {
+				System.out.println("caca");
+				anchowindow = MainWindow.getWidth();
+				menuBar.setBounds(0, 0, anchowindow, 21);
+				
+			}
+		});
 		MainWindow.setBounds(100, 100, 450, 300);
 		MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MainWindow.getContentPane().setLayout(null);
+		
+		menuBar.setAutoscrolls(true);
+		menuBar.setMaximumSize(new Dimension(anchowindow, 2));
+		menuBar.setBorder(UIManager.getBorder("MenuBar.border"));
+		menuBar.setBounds(0, 0, anchowindow, 21);
+		MainWindow.getContentPane().add(menuBar);
+		
+		JMenu mnArchivo = new JMenu("Archivo");
+		mnArchivo.setMinimumSize(new Dimension(60, 0));
+		menuBar.add(mnArchivo);
+		
+		JMenuItem mntmAbrir = new JMenuItem("Abrir");
+		mnArchivo.add(mntmAbrir);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Guardar");
+		mnArchivo.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu = new JMenu("Opciones");
+		mnNewMenu.setMinimumSize(new Dimension(100, 0));
+		menuBar.add(mnNewMenu);
 	}
-
 }
