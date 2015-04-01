054dd1a77c503dddde4747379fa7cfb27772b96a
franchi <franchi@JavierF.vinfra.local>
2015-04-01 13:01:23 +0200

    01/04/2015. Starting Proyect

--- HyCrop/src/App/MainWindow.java
+++ HyCrop/src/App/MainWindow.java
@@ -1,10 +1,43 @@
 package App;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class MainWindow {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private JFrame MainWindow;
 
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.MainWindow.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		MainWindow = new JFrame();
+		MainWindow.setBounds(100, 100, 450, 300);
+		MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
