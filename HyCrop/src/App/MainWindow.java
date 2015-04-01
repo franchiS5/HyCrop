5c04394d1fe77e6bec7441b5de201a61c571766e
franchi <franchi@JavierF.vinfra.local>
2015-04-01 18:29:02 +0200

    01/04/2015. Last Update. Starting proyect. Not working.

--- HyCrop/src/App/MainWindow.java
+++ HyCrop/src/App/MainWindow.java
@@ -1,20 +1,24 @@
 package App;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.UIManager;
-import javax.swing.JMenu;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+
+
 
 public class MainWindow {
 
 	private JFrame MainWindow;
 	private int anchowindow = 300;
+	public JTree tree;
 
 	/**
 	 * Launch the application.
@@ -42,9 +46,12 @@ public class MainWindow {
 	/**
 	 * Initialize the contents of the frame.
 	 */
+	@SuppressWarnings({ "serial", "rawtypes", "unchecked" })
 	private void initialize() {
 		MainWindow = new JFrame();
+		MainWindow.setTitle("HyCrop 1.0 Beta");
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 1008, 21);
 		MainWindow.addComponentListener(new ComponentAdapter() {
 			@Override
 			public void componentResized(ComponentEvent arg0) {
@@ -54,15 +61,16 @@ public class MainWindow {
 				
 			}
 		});
-		MainWindow.setBounds(100, 100, 450, 300);
+		MainWindow.setBounds(100, 100, 1024, 768);
 		MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		MainWindow.getContentPane().setLayout(null);
 		
 		menuBar.setAutoscrolls(true);
 		menuBar.setMaximumSize(new Dimension(anchowindow, 2));
 		menuBar.setBorder(UIManager.getBorder("MenuBar.border"));
-		menuBar.setBounds(0, 0, anchowindow, 21);
 		MainWindow.getContentPane().add(menuBar);
+		
+		
 		
 		JMenu mnArchivo = new JMenu("Archivo");
 		mnArchivo.setMinimumSize(new Dimension(60, 0));
@@ -74,8 +82,19 @@ public class MainWindow {
 		JMenuItem mntmNewMenuItem = new JMenuItem("Guardar");
 		mnArchivo.add(mntmNewMenuItem);
 		
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mnArchivo.add(mntmSalir);
+		
 		JMenu mnNewMenu = new JMenu("Opciones");
 		mnNewMenu.setMinimumSize(new Dimension(100, 0));
 		menuBar.add(mnNewMenu);
+		
+		JMenu mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+		
+		JMenuItem menuItem = new JMenuItem("?");
+		mnAyuda.add(menuItem);
+		
 	}
+	
 }
