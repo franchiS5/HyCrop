c2283f5bdb23e036150ebfe3330ac6a13be5abc4
franchi <franchi@JavierF.vinfra.local>
2015-04-14 17:52:43 +0200

    14/04/2015. Last Update. Testing

--- HyCrop/src/App/FileTreePanel.java
+++ HyCrop/src/App/FileTreePanel.java
@@ -1,145 +1,30 @@
 package App;
-
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.io.File;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 
-import javax.swing.Icon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.UIManager;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeNode;
 
-import App.FileTreePanel.FileTreeCellRenderer;
-import App.FileTreePanel.FileTreeNode;
-import javax.swing.JInternalFrame;
-import java.awt.Rectangle;
-
-
-
-public class MainWindow extends JPanel{
-
-	private JFrame MainWindow;
-	private int anchowindow = 300;
-	
-	/*
+/**
+ * @author Kirill Grouchnikov
+ */
+public class FileTreePanel extends JPanel {
+	/**
 	 * File system view.
 	 */
 	protected static FileSystemView fsv = FileSystemView.getFileSystemView();
 
-	
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainWindow window = new MainWindow();
-					window.MainWindow.setVisible(true);
-					
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	
-public MainWindow() {
-		initialize();
-}
-
-	
-	@SuppressWarnings({ })
-	private void initialize() {
-		MainWindow = new JFrame();
-		MainWindow.setTitle("HyCrop 1.0 Beta");
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 1008, 21);
-		MainWindow.addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent arg0) {
-				anchowindow = MainWindow.getWidth();
-				menuBar.setBounds(0, 0, anchowindow, 21);
-				
-			}
-		});
-		MainWindow.setBounds(100, 100, 1024, 768);
-		MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		MainWindow.getContentPane().setLayout(null);
-		
-		menuBar.setAutoscrolls(true);
-		menuBar.setMaximumSize(new Dimension(anchowindow, 2));
-		menuBar.setBorder(UIManager.getBorder("MenuBar.border"));
-		MainWindow.getContentPane().add(menuBar);
-		
-		
-		
-		JMenu mnArchivo = new JMenu("Archivo");
-		mnArchivo.setMinimumSize(new Dimension(60, 0));
-		menuBar.add(mnArchivo);
-		
-		JMenuItem mntmAbrir = new JMenuItem("Abrir");
-		mnArchivo.add(mntmAbrir);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Guardar");
-		mnArchivo.add(mntmNewMenuItem);
-		
-		JMenuItem mntmSalir = new JMenuItem("Salir");
-		mntmSalir.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				System.exit(0);
-			}
-		});
-		mnArchivo.add(mntmSalir);
-		
-		JMenu mnNewMenu = new JMenu("Opciones");
-		mnNewMenu.setMinimumSize(new Dimension(100, 0));
-		menuBar.add(mnNewMenu);
-		
-		JMenu mnAyuda = new JMenu("Ayuda");
-		menuBar.add(mnAyuda);
-		
-		JMenuItem menuItem = new JMenuItem("?");
-		mnAyuda.add(menuItem);
-		
-		
-		
-		//treeframe.getContentPane().add(new FileTreePanel());
-		
-		JInternalFrame internalFrame = new JInternalFrame("");
-		internalFrame.setFrameIcon(null);
-		internalFrame.setBorder(null);
-		internalFrame.setBounds(0, 22, 250, 500);
-		internalFrame.getContentPane().add(new FileTreePanel());
-		MainWindow.getContentPane().add(internalFrame);
-		internalFrame.setVisible(true);
-		
-		
-		
-	}
-	
-	private static class FileTreeCellRenderer extends DefaultTreeCellRenderer {
+	/**
+	 * Renderer for the file tree.
+	 * 
+	 * @author Kirill Grouchnikov
+	 */
+	public static class FileTreeCellRenderer extends DefaultTreeCellRenderer {
 		/**
 		 * Icon cache to speed the rendering.
 		 */
@@ -191,8 +76,13 @@ public MainWindow() {
 			return result;
 		}
 	}
-	
-	private static class FileTreeNode implements TreeNode {
+
+	/**
+	 * A node in the file tree.
+	 * 
+	 * @author Kirill Grouchnikov
+	 */
+	public static class FileTreeNode implements TreeNode {
 		/**
 		 * Node file.
 		 */
@@ -346,25 +236,30 @@ public MainWindow() {
 
 	/**
 	 * Creates the file tree panel.
-	 * @return 
 	 */
-	public void FileTreePanel() {
+	public FileTreePanel() {
 		this.setLayout(new BorderLayout());
 
 		File[] roots = File.listRoots();
 		FileTreeNode rootTreeNode = new FileTreeNode(roots);
-		tree = new JTree(rootTreeNode);
-		tree.setCellRenderer(new FileTreeCellRenderer());
-		tree.setRootVisible(false);
+		this.tree = new JTree(rootTreeNode);
+		this.tree.setCellRenderer(new FileTreeCellRenderer());
+		this.tree.setRootVisible(false);
 		final JScrollPane jsp = new JScrollPane(this.tree);
-		//jsp.setBorder(new EmptyBorder(0, 0, 0, 0));
-		jsp.setBounds(0, 25, 200, 500);
-		//add(jsp, BorderLayout.CENTER);
-		MainWindow.getContentPane().add(jsp);
+		jsp.setBorder(new EmptyBorder(0, 0, 0, 0));
+		this.add(jsp, BorderLayout.CENTER);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("File tree");
+				frame.setSize(500, 400);
+				frame.setLocationRelativeTo(null);
+				frame.add(new FileTreePanel());
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
 	}
 }
-		
-		
-		
-	
-
